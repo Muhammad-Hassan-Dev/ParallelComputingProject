c342606b1af3bb0321cd1bd9204652f5b1ac07cc
Muhammad-Hassan-Dev <mhassan8@mtu.edu>
2021-02-18 17:35:26 +0500

    Update: Threaded Search implementation

--- Main.cpp
+++ Main.cpp
@@ -1,4 +1,40 @@
 #include "SearchProblemSetup.cpp"
+#include "SearchChunk.cpp"
+#include <pthread.h>
+
+void LeadSearch(SearchTask * tasks, int threads_count)
+{
+	pthread_t * thread_ids = new pthread_t[threads_count];
+
+	int * statuses = new int[threads_count];
+
+	for(int i=0;i<threads_count;i++)
+	{
+		statuses[i] = pthread_create(&thread_ids[i], NULL, SearchChunk, (void*) &tasks[i]);
+
+		if(statuses[i] != 0)
+		{
+			cout <<"pthread_create failed."<< endl;
+			exit(-1); //if thread creation failed, exit the process
+		}
+	}
+
+	for(int i=0;i<threads_count;i++)
+	{
+		pthread_join(thread_ids[i], NULL);
+	}
+
+	int total_occurrences = 0;
+
+	for(int i=0;i<threads_count;i++)
+	{
+		total_occurrences += tasks[i].Occurrence_Count;
+	}
+
+	cout << endl << "RESULTS:---> " << tasks[0].SearchItem << " occurred in the array "<< total_occurrences << " times."<< endl;
+
+	return;
+}
 
 int main(int argc, char *argv[])
 {
@@ -33,5 +69,7 @@ int main(int argc, char *argv[])
 
 	SearchTask * search_tasks = GetRandomSearchTasks(threads_count, searchspace_size); //Get a random search problem in chunks
  
-	return 0;
+	LeadSearch(search_tasks, threads_count); //This will perform the search on each chunk in an individual thread and cumulate and print the result
+
+	pthread_exit(NULL);
 }
---
+++ SearchChunk.cpp
@@ -0,0 +1,20 @@
+void* SearchChunk(void* searchtask)
+{	
+	SearchTask task = *((SearchTask*) searchtask); //convert search task pointer to search task object
+
+	int * Data = task.Data;
+	int Size = task.Size;
+	int SearchItem = task.SearchItem;
+
+	int count = 0; //how many times search item occurs in the data chunk
+
+	for(int i = 0; i < Size; i ++)
+	{
+		if(Data[i] == SearchItem)
+			count ++;
+	}
+
+	((SearchTask*) searchtask)->Occurrence_Count = count;
+
+	pthread_exit(NULL);
+}
